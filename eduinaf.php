8a8672dcfc6c525b8437ec398ecf48ddc6d2e19c
ulaulaman <34242086+ulaulaman@users.noreply.github.com>
2018-02-05 14:03:55 +0100

    0.9.3 Integrazione con GitHub Updater
    Aggiunto il GitHub Plugin URI per l'installazione e l'aggiornamento tramite il GitHub Updater

--- eduinaf.php
+++ eduinaf.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Edu INAF Plugin
 Description: Il plugin aggiunge varie funzionalità al sito Edu INAF senza modificare direttamente il codice php del tema.
-Version: 0.9.2
+Version: 0.9.3
 Author: Gianluigi Filippelli
 Author URI: http://dropseaofulaula.blogspot.it/
-GitHub Plugin URI: 
+GitHub Plugin URI: https://github.com/ulaulaman/eduinaf
 License: GPLv2 or later
 */
 /* ------------------------------------------------------ */
