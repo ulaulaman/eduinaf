eba407224d961da0fc90e16a7f212c38edf4fc1b
ulaulaman <34242086+ulaulaman@users.noreply.github.com>
2018-03-23 23:51:40 +0100

    Sistemazione indentazione e cancellazione commenti superflui

--- incl/speciali.php
+++ incl/speciali.php
@@ -1,7 +1,5 @@
 <?php
 # tassonomia per gli speciali
-# guida https://code.tutsplus.com/tutorials/adding-post-series-functionality-to-wordpress-with-taxonomies--wp-25414
-
 function speciali_tax() {
 	$labels = array(
 		'name' => _x('Speciali', 'taxonomy general name'),
@@ -26,103 +24,85 @@ function speciali_tax() {
 		)
 	);
 }
-
 add_action('init', 'speciali_tax', 0);
 
-# shortcode per mostrare l'elenco degli articoli di uno speciale
-
+# shortcode per mostrare l'elenco degli articoli di uno speciale all'interno di un post o di una pagina
 function specialieduinaf($atts) {
-
-   global $post;
-
-   extract(
-      shortcode_atts(
-         array( 
-		'speciale' => 'null',
-                'titolo' => 'null',
-                'linkpage' => 'null',
-	 ),
-         $atts
-      )
-   );
-
-   $q = new WP_Query( array( 'speciali' => $speciale, 'posts_per_page'=>-1 ) );
-   $nnome = 'null';
-   if ( $titolo <> 'null' ) {
-     $nnome = $titolo;
-     if ( $linkpage <> 'null' ) {
-       $nnome = '<a href="'.$linkpage.'>'.$titolo.'</a>';
-     }
-   } else {
-     $nnome = $speciale;
-   }
-
-   $header = '<div style="float: right; padding: 5px; width: 25%;" class="divTable paleBlueRows"><div class="divTableHeading"><div class="divTableRow"><div class="divTableHead"><strong>Speciale '.$nnome.'</strong></div></div></div>';
-
-   if ( $q->have_posts() ) {
-	while ( $q->have_posts() ) {
-		$q->the_post();
-                $titolo = get_the_title();
-
-		$grid .= '<div class="divTableBody"><div class="divTableRow"><div class="divTableCell"><a href="'.get_the_permalink().'">'.$titolo.'</a></div>';
+	global $post;
+	
+	extract(
+		shortcode_atts(
+			array(
+				'speciale' => 'null',
+				'titolo' => 'null',
+				'linkpage' => 'null',
+			),
+			$atts
+		)
+	);
+	
+	$q = new WP_Query( array( 'speciali' => $speciale, 'posts_per_page'=>-1 ) );
+	$nnome = 'null';
+	if ( $titolo <> 'null' ) {
+		$nnome = $titolo;
+		if ( $linkpage <> 'null' ) {
+			$nnome = '<a href="'.$linkpage.'>'.$titolo.'</a>';
+		}
+	} else {$nnome = $speciale;}
+	
+	$header = '<div style="float: right; padding: 5px; width: 25%;" class="divTable paleBlueRows"><div class="divTableHeading"><div class="divTableRow"><div class="divTableHead"><strong>Speciale '.$nnome.'</strong></div></div></div>';
+	if ( $q->have_posts() ) {
+		while ( $q->have_posts() ) {
+			$q->the_post();
+			$titolo = get_the_title();
+			$grid .= '<div class="divTableBody"><div class="divTableRow"><div class="divTableCell"><a href="'.get_the_permalink().'">'.$titolo.'</a></div>';
+		}
+		$grid = $header.$grid.'</div></div>';
+		/* ripristino */
+		wp_reset_postdata();
 	}
-	$grid = $header.$grid.'</div></div>';
-	/* ripristino */
-	wp_reset_postdata();
-   }
-
-  $container = '<p><div>'.$grid.'</div></p>';
-
-  return $container;
-
+	
+	$container = '<p><div>'.$grid.'</div></p>';
+	
+	return $container;
 }
-
 add_shortcode( 'specialieduinaf', 'specialieduinaf' );
 
 # shortcode per la creazione della griglia per la pagina degli speciali
-
 function grigliaspeciali($atts) {
-
-   global $post;
-
-   extract(
-      shortcode_atts(
-         array( 
-		'speciale' => 'null',
-	 ),
-         $atts
-      )
-   );
-
-   $q = new WP_Query( array( 'speciali' => $speciale, 'posts_per_page'=>-1 ) );
-   $content = '<p>';
-
-   if ( $q->have_posts() ) {
-	while ( $q->have_posts() ) {
-		$q->the_post();
-                $titolo = get_the_title();
-                #$autori = coauthors_posts_links(", ", " e ", null, null, false); # funziona se installato co-authors plus
-                #$autori = coauthors_posts_links(", ", " e ", null, null, false);
-                if ( function_exists( 'get_coauthors' ) ) {
-		  $autori = coauthors_posts_links(", ", " e ", null, null, false);
+	global $post;
+	
+	extract(
+		shortcode_atts(
+			array(
+				'speciale' => 'null',
+			),
+			$atts
+		)
+	);
+	
+	$q = new WP_Query( array( 'speciali' => $speciale, 'posts_per_page'=>-1 ) );
+	$content = '<p>';
+	
+	if ( $q->have_posts() ) {
+		while ( $q->have_posts() ) {
+			$q->the_post();
+			$titolo = get_the_title();
+			if ( function_exists( 'get_coauthors' ) ) {
+				$autori = coauthors_posts_links(", ", " e ", null, null, false);
+			} else {$autori = the_author();}
+			
+			$estratto = get_the_excerpt();
+			$tassonomia = get_the_category_list();
+			$header = '<h4 style="color: #ecb252;">'.$titolo.'</h4><hr/>';
+			$content .= $header.'<p><em>di <strong>'.$autori.'</strong><br/>'.$tassonomia.'</em><br/>'.$estratto.'<br/>(<a href="'.get_the_permalink().'" style="color: #1d71b8;">continua</a>)</p>';
 		}
-		else
-		{
-		  $autori = the_author();
-		}
-                $estratto = get_the_excerpt();
-                $tassonomia = get_the_category_list();
-
-		$header = '<h4 style="color: #ecb252;">'.$titolo.'</h4><hr/>';
-                $content .= $header.'<p><em>di <strong>'.$autori.'</strong><br/>'.$tassonomia.'</em><br/>'.$estratto.'<br/>(<a href="'.get_the_permalink().'" style="color: #1d71b8;">continua</a>)</p>';
+		
+		$content = $content.'</p>';
+		/* ripristino */
+		wp_reset_postdata();
 	}
-        $content = $content.'</p>';
-	/* ripristino */
-	wp_reset_postdata();
-   }
-
-  return $content;
-
+	
+	return $content;
 }
-
 add_shortcode( 'grigliaspeciali', 'grigliaspeciali' );
