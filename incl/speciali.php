6111e62dc2a64705e0e3373b353790d3d830b90a
ulaulaman <34242086+ulaulaman@users.noreply.github.com>
2018-03-28 02:20:59 +0200

    Sostituito lo shortcode della tabella speciali

--- incl/speciali.php
+++ incl/speciali.php
@@ -26,48 +26,6 @@ function speciali_tax() {
 }
 add_action('init', 'speciali_tax', 0);
 
-# shortcode per mostrare l'elenco degli articoli di uno speciale all'interno di un post o di una pagina
-function specialieduinaf($atts) {
-	global $post;
-	
-	extract(
-		shortcode_atts(
-			array(
-				'speciale' => 'null',
-				'titolo' => 'null',
-				'linkpage' => 'null',
-			),
-			$atts
-		)
-	);
-	
-	$q = new WP_Query( array( 'speciali' => $speciale, 'posts_per_page'=>-1 ) );
-	$nnome = 'null';
-	if ( $titolo <> 'null' ) {
-		$nnome = $titolo;
-		if ( $linkpage <> 'null' ) {
-			$nnome = '<a href="'.$linkpage.'>'.$titolo.'</a>';
-		}
-	} else {$nnome = $speciale;}
-	
-	$header = '<div style="float: right; padding: 5px; width: 25%;" class="divTable paleBlueRows"><div class="divTableHeading"><div class="divTableRow"><div class="divTableHead"><strong>Speciale '.$nnome.'</strong></div></div></div>';
-	if ( $q->have_posts() ) {
-		while ( $q->have_posts() ) {
-			$q->the_post();
-			$titolo = get_the_title();
-			$grid .= '<div class="divTableBody"><div class="divTableRow"><div class="divTableCell"><a href="'.get_the_permalink().'">'.$titolo.'</a></div>';
-		}
-		$grid = $header.$grid.'</div></div>';
-		/* ripristino */
-		wp_reset_postdata();
-	}
-	
-	$container = '<p><div>'.$grid.'</div></p>';
-	
-	return $container;
-}
-add_shortcode( 'specialieduinaf', 'specialieduinaf' );
-
 # shortcode per la creazione della griglia per la pagina degli speciali
 function grigliaspeciali($atts) {
 	global $post;
@@ -93,9 +51,8 @@ function grigliaspeciali($atts) {
 			} else {$autori = the_author();}
 			
 			$estratto = get_the_excerpt();
-			$tassonomia = get_the_category_list();
 			$header = '<h4 style="color: #ecb252;">'.$titolo.'</h4><hr/>';
-			$content .= $header.'<p><em>di <strong>'.$autori.'</strong><br/>'.$tassonomia.'</em><br/>'.$estratto.'<br/>(<a href="'.get_the_permalink().'" style="color: #1d71b8;">continua</a>)</p>';
+			$content .= $header.'<p><em>di <strong>'.$autori.'</strong></em><br/>'.$estratto.'<br/>(<a href="'.get_the_permalink().'" style="color: #1d71b8;">continua</a>)</p>';
 		}
 		
 		$content = $content.'</p>';
@@ -106,3 +63,50 @@ function grigliaspeciali($atts) {
 	return $content;
 }
 add_shortcode( 'grigliaspeciali', 'grigliaspeciali' );
+
+#shortcode per mostrare in una tabella l'elenco degli articoli di uno speciale: da utilizzare in un widget di testo in attesa di creare un widget vero e proprio
+function specialishort($atts) {
+	
+	global $post;
+	
+	extract(
+		shortcode_atts(
+			array(
+				'speciale' => 'null',
+			),
+			$atts
+		)
+	);
+	
+	#tutti i termini associati all'eventuale speciale associato al post
+	$term_list = wp_get_post_terms($post->ID, 'speciali', array("fields" => "all"));
+	#estrazione del nome dello speciale associato al post
+	$nomespeciale = $term_list[0]->name;
+	
+	if ($nomespeciale <> $speciale) { $content = null; } else {
+		$q = new WP_Query( array( 'speciali' => $speciale, 'posts_per_page'=>-1 ) );
+		$content = '<p>';
+		
+		if ( $q->have_posts() ) {
+			while ( $q->have_posts() ) {
+				$q->the_post();
+				$titolo = get_the_title();
+				if ( function_exists( 'get_coauthors' ) ) {
+					$autori = coauthors_posts_links(", ", " e ", null, null, false);
+				} else {
+					$autori = the_author();
+				}
+				$estratto = get_the_excerpt();
+				
+				$header = '<div class="divTable paleBlueRows"><div class="divTableHeading"><div class="divTableRow"><div class="divTableHead"><strong>Speciale '.$speciale.'</strong></div></div></div>';
+				$content .= $header.'<div class="divTableBody"><div class="divTableRow"><div class="divTableCell"><a href="'.get_the_permalink().'" style="color: #1d71b8;">'.$titolo.'</a></div></div></div></div>';
+			}
+			$content = $content.'</p>';
+			/* ripristino */
+			wp_reset_postdata();
+		}
+	}
+	
+	return $content;
+}
+add_shortcode( 'specialishort', 'specialishort' );
