690620532768377f840ed2cb9fd08aced49145a2
ulaulaman <gianluigi.ulaula@gmail.com>
2020-10-23 22:43:59 +0200

    Aggiunto shortcode per loop articoli con categorie e tag come variabili

--- incl/grid.php
+++ incl/grid.php
@@ -1,13 +1,78 @@
 <?php
 # guide utilizzate: https://www.zenwebthemes.com/blog/create-grid-layout-wordpress-flexbox/
 # https://wordpress.stackexchange.com/questions/269671/wp-query-by-keyword-or-post-tag
-# inclusione di grid.css
-# function edu_inaf_to_the_head () {
-#   wp_register_style( 'grid', plugins_url( 'eduinaf/incl/grid.css' ) );
-#   wp_enqueue_style( 'grid' );
-# }
-#
-#add_action( 'wp_enqueue_scripts', 'edu_inaf_to_the_head' );
+
+# shortcode per la creazione di un loop di articoli in funzione dei categoria e tag
+function postlooptab($atts) {
+	global $post;
+	
+	extract(
+		shortcode_atts(
+			array(
+				'categoria' => null,
+				'tag' => null,
+				'pag' => null,
+			),
+			$atts
+		)
+	);
+	
+	if ( $pag <> null ) {
+		if ( $categoria <> null ) {
+			if ( $tag <> null) {
+				$q = new WP_Query ( array( 'post_type' => 'post', 'category_name' => $categoria, 'tag' => $tag, 'posts_per_page' => $pag ) );
+			} else {
+				$q = new WP_Query ( array( 'post_type' => 'post', 'category_name' => $categoria, 'posts_per_page' => $pag ) );
+			}
+		} else {
+			if ( $tag <> null ) {
+				$q = new WP_Query ( array( 'post_type' => 'post', 'tag' => $tag, 'posts_per_page' => $pag ) );
+			} else {
+				$q = new WP_Query ( array( 'post_type' => 'post', 'posts_per_page' => $pag ) );
+			}
+		}
+	} else {
+		if ( $categoria <> null ) {
+			if ( $tag <> null ) {
+				$q = new WP_Query ( array( 'post_type' => 'post', 'category_name' => $categoria, 'tag' => $tag, 'posts_per_page' => 5 ) );
+			} else {
+				$q = new WP_Query ( array( 'post_type' => 'post', 'category_name' => $categoria, 'posts_per_page' => $pag ) );
+			}
+		} else {
+			if ( $tag <> null ) {
+				$q = new WP_Query ( array( 'post_type' => 'post', 'tag' => $tag, 'posts_per_page' => $pag ) );
+			} else {
+				$q = new WP_Query ( array( 'post_type' => 'post', 'posts_per_page' => $pag ) );
+			}
+		}
+	}
+	
+	$content = null;
+	
+	if ( $q->have_posts() ) {
+		while ( $q->have_posts() ) {
+			$q->the_post();
+			$titolo = get_the_title();
+			
+			$content .= '<li><a href="'.get_the_permalink().'">'.$titolo.'</a></li>';
+		}
+	
+		/* ripristino */
+		wp_reset_postdata();
+		
+	}
+	
+	if ( $content <> null ) {
+		$content = '<div id="recent-posts-2" class="widget widget_recent_entries"><h4 class="widget-title h6"><span>Ultimi articoli</span></h4><ul>'.$content.'</ul></div>';
+	} else {
+		$content = '<div id="recent-posts-2" class="widget widget_recent_entries"><h4 class="widget-title h6"><span>Ultimi articoli</span></h4><ul><li>Nessun articolo presente in archivio</li></ul></div><p></p>';
+	}
+	
+	$out = $content.$after_widget;
+	
+	return $out;
+}
+add_shortcode( 'postlooptab', 'postlooptab' );
 
 # griglia per loop generico per categoria ed etichetta
 add_shortcode ( 'grigliaeduinaf', 'grigliaeduinaf');
