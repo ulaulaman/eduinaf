3bc12f1719ce5cd54ec18ba8f5937b4b1245066b
ulaulaman <gianluigi.ulaula@gmail.com>
2020-11-19 22:09:55 +0100

    Attivata sidebar corso base e aggiornamento versione: 2020.1119

--- eduinaf.php
+++ eduinaf.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Edu INAF Tools
 Description: Il plugin aggiunge varie funzionalità al sito Edu INAF senza modificare direttamente il codice php del tema.
-Version: 2020.1111
+Version: 2020.1119
 Author: Gianluigi Filippelli
 Author URI: http://dropseaofulaula.blogspot.it/
 Plugin URI: https://ulaulaman.github.io/eduinaf/
@@ -26,6 +26,7 @@ require_once( EDUINAF__PLUGIN_DIR . 'incl/grid.php' );
 require_once( EDUINAF__PLUGIN_DIR . 'sidebars/astrodidattica.php' );
 require_once( EDUINAF__PLUGIN_DIR . 'sidebars/astrofoto.php' );
 require_once( EDUINAF__PLUGIN_DIR . 'sidebars/costellazioni.php' );
+require_once( EDUINAF__PLUGIN_DIR . 'sidebars/brera.php' );
 require_once( EDUINAF__PLUGIN_DIR . 'sidebars/widgets.php' );
 # Speciali
 require_once( EDUINAF__PLUGIN_DIR . 'incl/speciali.php' );
