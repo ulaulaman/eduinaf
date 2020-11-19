5bc610a2cf38fb87e86eafef86a37d6c60c4fb07
ulaulaman <gianluigi.ulaula@gmail.com>
2020-11-19 22:08:36 +0100

    Aggiunto il widget del corso base di astronomia

--- sidebars/widgets.php
+++ sidebars/widgets.php
@@ -110,3 +110,57 @@ add_action( 'widgets_init', 'register_stars' );
 function register_stars() { 
     register_widget( 'Widget_Stars' ); 
 }
+
+# corso base
+class Widget_Brera extends WP_Widget {
+ 
+	/** Registrazione del widget */
+    public function __construct() {
+        parent::__construct(
+            'widget_brera', // Base ID
+            'Widget Brera', // Name
+            array( 'description' => __( 'Widget per l\'inserimento della sidebar del corso base di astronomia', 'text_domain' ), ) // Args
+        );
+    }
+ 
+    /** Front-end del widget */
+    public function widget( $args, $instance ) {
+        extract( $args );
+        $shortcode = do_shortcode('[sbcorsobase]');
+        echo $shortcode;
+        echo $menu;
+        echo $after_widget;
+    }
+ 
+    /** Back-end del widget */
+    public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        }
+        else {
+            $title = __( 'Titolo', 'text_domain' );
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Titolo:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+         </p>
+    <?php
+    }
+ 
+    /** Ripulisce i valori del widget man mano che vengono salvati */
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( !empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+ 
+        return $instance;
+    }
+ 
+} // class Widget_Brera
+
+// Registra Widget_Brera
+add_action( 'widgets_init', 'register_brera' );
+     
+function register_brera() { 
+    register_widget( 'Widget_Brera' ); 
+}
