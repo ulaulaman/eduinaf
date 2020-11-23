31260cbf669ac810d9b5350c10c4d5681e776f85
ulaulaman <gianluigi.ulaula@gmail.com>
2020-11-24 00:23:05 +0100

    Aggiunto widget astrofoto

--- sidebars/widgets.php
+++ sidebars/widgets.php
@@ -128,7 +128,6 @@ class Widget_Brera extends WP_Widget {
         extract( $args );
         $shortcode = do_shortcode('[sbcorsobase]');
         echo $shortcode;
-        echo $menu;
         echo $after_widget;
     }
  
@@ -163,4 +162,57 @@ add_action( 'widgets_init', 'register_brera' );
      
 function register_brera() { 
     register_widget( 'Widget_Brera' ); 
+}
+
+# astrofoto
+class Widget_AstroFoto extends WP_Widget {
+ 
+	/** Registrazione del widget */
+    public function __construct() {
+        parent::__construct(
+            'widget_astrofoto', // Base ID
+            'Widget Astro Foto', // Name
+            array( 'description' => __( 'Widget per l\'inserimento della sidebar delle fotografie astronomiche', 'text_domain' ), ) // Args
+        );
+    }
+ 
+    /** Front-end del widget */
+    public function widget( $args, $instance ) {
+        extract( $args );
+        $shortcode = do_shortcode('[sbastrofoto]');
+        echo $shortcode;
+        echo $after_widget;
+    }
+ 
+    /** Back-end del widget */
+    public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        }
+        else {
+            $title = __( 'Titolo', 'text_domain' );
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Titolo:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+         </p>
+    <?php
+    }
+ 
+    /** Ripulisce i valori del widget man mano che vengono salvati */
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( !empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+ 
+        return $instance;
+    }
+ 
+} // class Widget_AstroFoto
+
+// Registra Widget_AstroFoto
+add_action( 'widgets_init', 'register_astrofoto' );
+     
+function register_astrofoto() { 
+    register_widget( 'Widget_AstroFoto' ); 
 }
