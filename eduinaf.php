b60d9a11be85cad920abd95d218a6b1b93ed517e
ulaulaman <gianluigi.ulaula@gmail.com>
2021-06-30 10:38:09 +0200

    Aggiornamento versione: 2021.0629

--- eduinaf.php
+++ eduinaf.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Edu INAF Tools
 Description: Il plugin aggiunge varie funzionalità al sito Edu INAF senza modificare direttamente il codice php del tema.
-Version: 2021.0628
+Version: 2021.0629
 Author: Gianluigi Filippelli
 Author URI: http://dropseaofulaula.blogspot.it/
 Plugin URI: https://ulaulaman.github.io/#EduINAF
