bd488be444b0d787aceeb96043eb209f7299f145
Gianluigi <gianluigi.ulaula@gmail.com>
2023-10-19 16:02:54 +0200

    Aggiornamento versione: 2023.1019

--- eduinaf.php
+++ eduinaf.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Edu INAF Tools
 Description: Il plugin aggiunge varie funzionalità al sito Edu INAF senza modificare direttamente il codice php del tema.
-Version: 2023.0113
+Version: 2023.1019
 Author: Gianluigi Filippelli
 Author URI: http://dropseaofulaula.blogspot.it/
 Plugin URI: https://ulaulaman.github.io/#EduINAF
